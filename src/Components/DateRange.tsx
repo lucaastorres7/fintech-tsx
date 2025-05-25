2aaa6443bd3cc0b625e08445a374824bd6a355e9
Lucas Torres <lucastorreslr10@gmail.com>
2025-05-25 16:12:35 -0300

    feat: add responsive date and default value

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import DateInput from "./DateInput";
+import { useData } from "../Context/DataContext";
 
 const DateRange = () => {
-  const [start, setStart] = React.useState("");
-  const [end, setEnd] = React.useState("");
+  const { start, setStart, end, setEnd } = useData();
 
   return (
     <form className="box flex" onSubmit={(e) => e.preventDefault()}>
--- src/Context/DataContext.tsx
+++ src/Context/DataContext.tsx
@@ -15,6 +15,10 @@ type IDataContext = {
   data: ISale[] | null;
   loading: boolean;
   error: string | null;
+  start: string;
+  setStart: React.Dispatch<React.SetStateAction<string>>;
+  end: string;
+  setEnd: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const DataContext = React.createContext<IDataContext | null>(null);
@@ -25,13 +29,27 @@ export const useData = () => {
   return context;
 };
 
+function getDateRange(daysAgo: number) {
+  const date = new Date();
+  date.setDate(date.getDate() - daysAgo);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear());
+  return `${year}-${month}-${day}`;
+}
+
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [start, setStart] = React.useState(getDateRange(30)); //30 days ago
+  const [end, setEnd] = React.useState(getDateRange(0)); // Today
+
   const { data, loading, error } = useFetch<ISale[]>(
-    "https://data.origamid.dev/vendas/"
+    `https://data.origamid.dev/vendas/?inicio=${start}&final=${end}`
   );
 
   return (
-    <DataContext.Provider value={{ data, loading, error }}>
+    <DataContext.Provider
+      value={{ data, loading, error, start, setStart, end, setEnd }}
+    >
       {children}
     </DataContext.Provider>
   );
