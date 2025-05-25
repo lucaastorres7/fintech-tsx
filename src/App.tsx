200de64d68ae7148d618643165eac8f042a37220
Lucas Torres <lucastorreslr10@gmail.com>
2025-05-25 17:37:07 -0300

    feat: add sidenav and react-router to navigate pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import Header from "./Components/Header";
 import SideNav from "./Components/SideNav";
 import { DataContextProvider } from "./Context/DataContext";
 import Sales from "./Pages/Sales";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sale from "./Pages/Sale";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <SideNav />
-        <main>
-          <Header />
-          <Summary />
-          <Sales />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <SideNav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Summary />} />
+              <Route path="/sales" element={<Sales />} />
+              <Route path="/sales/:id" element={<Sale />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import DateRange from "./DateRange";
 import Months from "./Month";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const [title, setTitle] = React.useState("Resumo");
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    if (pathname === "/") {
+      setTitle("Resumo");
+      document.title = "Fintech | Resumo";
+    } else if (pathname === "/sales") {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    }
+  }, [pathname]);
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+        <h1 className="box bg3">{title}</h1>
       </div>
 
       <Months />
