20fef6cc19efd70bf3509cd7e9a2443b2fd6c95d
Lucas Torres <lucastorreslr10@gmail.com>
2025-05-25 16:49:45 -0300

    feat: add sales info on summary page

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useData } from "../Context/DataContext";
 
 const Summary = () => {
-  return <div>Summary</div>;
+  const { data } = useData();
+
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="summary flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .filter((item) => item.status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((item) => item.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+
+      <div className="box">Gráficos</div>
+    </section>
+  );
 };
 
 export default Summary;
